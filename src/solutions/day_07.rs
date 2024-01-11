28a8ccb690305ca35d809058e4c7d317e5293c42
Bram Inniger <bram@inniger.be>
2024-01-11 11:32:01 +0200

    Solved D07P02

--- src/solutions/day_07.rs
+++ src/solutions/day_07.rs
@@ -1,5 +1,12 @@
+use rustc_hash::FxHashSet;
+use std::ops::Not;
+
 pub fn solve_1(ips: &[&str]) -> usize {
     ips.iter().filter(|ip| supports_tls(ip)).count()
+}
+
+pub fn solve_2(ips: &[&str]) -> usize {
+    ips.iter().filter(|ip| supports_sls(ip)).count()
 }
 
 fn supports_tls(ip: &str) -> bool {
@@ -29,6 +36,31 @@ fn supports_tls(ip: &str) -> bool {
     supports
 }
 
+fn supports_sls(ip: &str) -> bool {
+    let bytes = ip.as_bytes();
+    let mut aba_s: FxHashSet<[u8; 2]> = FxHashSet::default();
+    let mut bab_s: FxHashSet<[u8; 2]> = FxHashSet::default();
+    let mut inside_hypernet = false;
+
+    for idx in 0..bytes.len() - 2 {
+        match bytes[idx] {
+            b'[' => inside_hypernet = true,
+            b']' => inside_hypernet = false,
+            _ => {
+                if bytes[idx] == bytes[idx + 2] && bytes[idx] != bytes[idx + 1] {
+                    if inside_hypernet {
+                        bab_s.insert([bytes[idx + 1], bytes[idx]]);
+                    } else {
+                        aba_s.insert([bytes[idx], bytes[idx + 1]]);
+                    }
+                }
+            }
+        }
+    }
+
+    aba_s.is_disjoint(&bab_s).not()
+}
+
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
@@ -54,5 +86,21 @@ mod tests {
             .collect_vec();
 
         assert_eq!(110, solve_1(&input));
+    }
+
+    #[test]
+    fn day_07_part_02_sample() {
+        let sample = vec!["aba[bab]xyz", "xyx[xyx]xyx", "aaa[kek]eke", "zazbz[bzb]cdb"];
+
+        assert_eq!(3, solve_2(&sample));
+    }
+
+    #[test]
+    fn day_07_part_02_solution() {
+        let input = include_str!("../../inputs/day_07.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(242, solve_2(&input));
     }
 }
