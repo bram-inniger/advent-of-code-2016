65cfebd753db10bf240a170d30a99e9a96fe44b3
Bram Inniger <bram@inniger.be>
2024-01-13 22:43:09 +0200

    Solved D16P01

---
+++ src/solutions/day_16.rs
@@ -0,0 +1,80 @@
+use itertools::Itertools;
+
+pub fn solve_1(data: &str, disk_size: usize) -> String {
+    DragonCurve::new(data).enlarge(disk_size).checksum()
+}
+
+#[derive(Debug)]
+struct DragonCurve {
+    data: String,
+}
+
+impl DragonCurve {
+    fn new(data: &str) -> Self {
+        Self {
+            data: data.to_string(),
+        }
+    }
+
+    fn enlarge(&self, size: usize) -> Self {
+        let mut a = self.data.clone();
+
+        while a.len() < size {
+            let b: String = a
+                .chars()
+                .rev()
+                .map(|c| match c {
+                    '0' => '1',
+                    '1' => '0',
+                    _ => unreachable!(),
+                })
+                .collect();
+
+            a = format!("{}0{}", a, b);
+        }
+
+        Self {
+            data: a.chars().take(size).collect(),
+        }
+    }
+
+    fn checksum(&self) -> String {
+        let mut checksum = self.data.clone();
+
+        while checksum.len() % 2 == 0 {
+            checksum = checksum
+                .chars()
+                .chunks(2)
+                .into_iter()
+                .map(|c| {
+                    let pair: String = c.collect();
+                    match pair.as_str() {
+                        "00" | "11" => '1',
+                        "01" | "10" => '0',
+                        _ => unreachable!(),
+                    }
+                })
+                .collect()
+        }
+
+        checksum
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day_16_part_01_sample() {
+        assert_eq!("100", solve_1("110010110100", 12));
+        assert_eq!("01100", solve_1("10000", 20));
+    }
+
+    #[test]
+    fn day_16_part_01_solution() {
+        let input = include_str!("../../inputs/day_16.txt").trim();
+
+        assert_eq!("10101001010100001", solve_1(input, 272));
+    }
+}
