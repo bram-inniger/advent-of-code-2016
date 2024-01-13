da9ae99d0d19229e16f3c93b4499538de136b887
Bram Inniger <bram@inniger.be>
2024-01-13 16:44:17 +0200

    Solved D14P02

--- src/solutions/day_14.rs
+++ src/solutions/day_14.rs
@@ -3,8 +3,16 @@ use md5::{Digest, Md5};
 use rustc_hash::{FxHashMap, FxHashSet};
 
 pub fn solve_1(salt: &str) -> usize {
+    solve(salt, &Hash::single)
+}
+
+pub fn solve_2(salt: &str) -> usize {
+    solve(salt, &Hash::stretched)
+}
+
+fn solve(salt: &str, hasher: &dyn Fn(&str, usize) -> Hash) -> usize {
     let mut idx = 0;
-    let mut hashes = (0..=1_000).map(|idx| Hash::new(salt, idx)).collect_vec();
+    let mut hashes = (0..=1_000).map(|idx| hasher(salt, idx)).collect_vec();
     let mut next_quints: FxHashMap<char, usize> = (1..=1_000)
         .flat_map(|idx| &hashes[idx].quintuplets)
         .group_by(|&c| c)
@@ -26,7 +34,7 @@ pub fn solve_1(salt: &str) -> usize {
         }
 
         idx += 1;
-        hashes.push(Hash::new(salt, idx + 1_000));
+        hashes.push(hasher(salt, idx + 1_000));
 
         hashes[idx].quintuplets.iter().for_each(|q| {
             *next_quints.get_mut(q).unwrap() -= 1;
@@ -44,10 +52,30 @@ struct Hash {
 }
 
 impl Hash {
-    fn new(salt: &str, idx: usize) -> Self {
+    fn single(salt: &str, idx: usize) -> Self {
         let mut hasher = Md5::new();
         hasher.update(format!("{}{}", salt, idx));
-        let result = hex::encode(hasher.finalize()).chars().collect_vec();
+        let hash = hasher.finalize();
+
+        Self::from_hash(hex::encode(hash))
+    }
+
+    fn stretched(salt: &str, idx: usize) -> Self {
+        let mut hasher = Md5::new();
+        hasher.update(format!("{}{}", salt, idx));
+        let mut hash = hex::encode(hasher.finalize());
+
+        for _ in 0..2_016 {
+            let mut hasher = Md5::new();
+            hasher.update(hash);
+            hash = hex::encode(hasher.finalize());
+        }
+
+        Self::from_hash(hash)
+    }
+
+    fn from_hash(hash: String) -> Hash {
+        let result = hash.chars().collect_vec();
 
         let triples = (0..result.len() - 2)
             .find(|&c| (c + 1..c + 3).all(|n| result[c] == result[n]))
@@ -80,5 +108,21 @@ mod tests {
         let input = include_str!("../../inputs/day_14.txt").trim();
 
         assert_eq!(15_168, solve_1(input));
+    }
+
+    #[ignore = "slow brute force test"]
+    #[test]
+    fn day_14_part_02_sample() {
+        let salt = "abc";
+
+        assert_eq!(22_551, solve_2(salt));
+    }
+
+    #[ignore = "slow brute force test"]
+    #[test]
+    fn day_14_part_02_solution() {
+        let input = include_str!("../../inputs/day_14.txt").trim();
+
+        assert_eq!(20_864, solve_2(input));
     }
 }
