bf4aa27bee4f2f127603d8fc9fdcd3300b20f933
Bram Inniger <bram@inniger.be>
2024-01-13 19:53:15 +0200

    Solved D15P01

---
+++ src/solutions/day_15.rs
@@ -0,0 +1,72 @@
+use itertools::Itertools;
+use std::str::FromStr;
+
+use regex::Regex;
+
+pub fn solve_1(disks: &[&str]) -> u32 {
+    let disks = disks.iter().map(|d| Disk::new(d)).collect_vec();
+    let mut time = 0;
+
+    loop {
+        let all_zero = disks
+            .iter()
+            .enumerate()
+            .all(|(idx, d)| d.position(time + idx as u32 + 1) == 0);
+
+        if all_zero {
+            return time;
+        }
+
+        time += 1
+    }
+}
+
+#[derive(Debug)]
+struct Disk {
+    positions: u32,
+    offset: u32,
+}
+
+impl Disk {
+    fn position(&self, time: u32) -> u32 {
+        (time + self.offset) % self.positions
+    }
+}
+
+impl Disk {
+    fn new(disk: &str) -> Self {
+        let re = Regex::new(r"^Disc #(:?\d+) has (?<positions>\d+) positions; at time=0, it is at position (?<offset>\d+)\.$").unwrap();
+        let caps = re.captures(disk).unwrap();
+
+        let positions = u32::from_str(caps.name("positions").unwrap().as_str()).unwrap();
+        let offset = u32::from_str(caps.name("offset").unwrap().as_str()).unwrap();
+
+        Self { positions, offset }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+
+    use super::*;
+
+    #[test]
+    fn day_15_part_01_sample() {
+        let sample = vec![
+            "Disc #1 has 5 positions; at time=0, it is at position 4.",
+            "Disc #2 has 2 positions; at time=0, it is at position 1.",
+        ];
+
+        assert_eq!(5, solve_1(&sample));
+    }
+
+    #[test]
+    fn day_15_part_01_solution() {
+        let input = include_str!("../../inputs/day_15.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(122_318, solve_1(&input));
+    }
+}
